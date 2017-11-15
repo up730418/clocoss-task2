86150c5a06755aa8c812aa6c2e354a9e8ca5e451
up730418 <up730418@port.ac.uk>
2017-11-15 19:49:20 +0000

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,52 +7,51 @@ const db = require('./db-datastore');
 
 //Get a list of all names stored
 api.get('/', async (req, res) => {
-	  try {
-		      res.json(await db.list());
-		    } catch (e) {
-			        console.error(e);
-			        res.sendStatus(500);
-			      }
+  try {
+    res.json(await db.list());
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
 });
 
 //Get a name and its  number
 api.get('/:id(\\w+)', async (req, res) => {
-	  try {
-		      res.send(await db.get(req.params.id));
-		    } catch (e) {
-			        console.error(e);
-			        res.sendStatus(500);
-			      }
+  try {
+    res.send(await db.get(req.params.id));
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
 });
 
 //Sets a names value to number inputed
 api.put('/:id(\\w+)', bodyParser.text(),, async (req, res) => {
-          try {
-                      res.send(await db.put(req.params.id, req.body));
-                    } catch (e) {
-                                console.error(e);
-                                res.sendStatus(500);
-                              }
+  try {
+    res.send(await db.put(req.params.id, req.body));
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
 });
 
 // Add a new name or update an existing name
 api.post('/:id(\\w+)', bodyParser.text(), async (req, res) => {
-	  try {
-		      res.send(await db.post(req.params.id, req.body));
-		      
-		    } catch (e) {
-			        console.error(e);
-			        res.sendStatus(500);
-			      }
+  try {
+    res.send(await db.post(req.params.id, req.body));
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
 });
 
 //Remove a name
 api.delete('/:id(\\w+)', async (req, res) => {
-          try {
-                      await db.delete(req.params.id);
-                      res.sendStatus(204);
-                    } catch (e) {
-                                console.error(e);
-                                res.sendStatus(500);
-                              }
+  try {
+    await db.delete(req.params.id);
+    res.sendStatus(204);
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
 });
